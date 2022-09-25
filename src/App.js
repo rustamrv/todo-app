ebc43b24c73fb8003be7fa9aa2d1e52ff3cb16bd
rustamrv <rystamrv@gmail.com>
2022-09-25 14:47:20 +0300

    add list

--- src/App.js
+++ src/App.js
@@ -1,9 +1,42 @@
+import List from "./components/List";
+import listSvg from './assets/img/list.svg';
+
+import './index.scss';
 
 function App() {
   return (
+    <div className='todo'>
+      <div className="sidebar">
+        <List items={[
+          {
+            icon: <img src={listSvg} alt="List icon" />,
+            name: "All tasks",
+            active: true
+          }, 
+        ]} />
+          <List items={[  
+          {
+            color: 'green',
+            name: "Покупки",
+            active: false
+          },
+          {
+            color: 'blue',
+            name: "Фронтенд",
+            active: false
+          },
+          {
+            color: 'pink',
+            name: "Фильмы и сериалы",
+            active: false
+          },
+        ]} />
+      </div>
+      <div className="content">
 
+      </div>
+    </div>
   );
 }
 
 export default App;
- 
